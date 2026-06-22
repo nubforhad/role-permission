697962a7dbdbe9df9eeea4bdcc6b4c0c765d8edd
nubforhad <nub.forhad@gmail.com>
2026-06-22 10:07:20 +0600

    sidebar header change this file code

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -1,53 +1,73 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Create New Role</h2>
+
+<div class="page-wrapper">
+    <div class="page-content">
+
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Create New Role</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary btn-sm mb-2"
+                       href="{{ route('roles.index') }}">
+                        <i class="fa fa-arrow-left"></i> Back
+                    </a>
+                </div>
+            </div>
         </div>
-        <div class="pull-right">
-            <a class="btn btn-primary btn-sm mb-2" href="{{ route('roles.index') }}"><i class="fa fa-arrow-left"></i> Back</a>
+
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="card">
+            <div class="card-body">
+
+                <form method="POST" action="{{ route('roles.store') }}">
+                    @csrf
+
+                    <div class="mb-3">
+                        <label class="form-label">Role Name</label>
+                        <input type="text" name="name" class="form-control">
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label">Permissions</label>
+
+                        @foreach($permission as $value)
+                            <div class="form-check">
+                                <input class="form-check-input"
+                                       type="checkbox"
+                                       name="permission[{{$value->id}}]"
+                                       value="{{$value->id}}">
+
+                                <label class="form-check-label">
+                                    {{ $value->name }}
+                                </label>
+                            </div>
+                        @endforeach
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">
+                        Submit
+                    </button>
+
+                </form>
+
+            </div>
         </div>
+
     </div>
 </div>
 
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-        </ul>
-    </div>
-@endif
-
-<form method="POST" action="{{ route('roles.store') }}">
-    @csrf
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="name" placeholder="Name" class="form-control">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Permission:</strong>
-                <br/>
-                @foreach($permission as $value)
-                    <label><input type="checkbox" name="permission[{{$value->id}}]" value="{{$value->id}}" class="name">
-                    {{ $value->name }}</label>
-                <br/>
-                @endforeach
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <button type="submit" class="btn btn-primary btn-sm mb-3"><i class="fa-solid fa-floppy-disk"></i> Submit</button>
-        </div>
-    </div>
-</form>
-
-<p class="text-center text-primary"><small>Design by 888</small></p>
 @endsection
